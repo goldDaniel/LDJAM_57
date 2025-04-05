9b28344a68bb26dfaa07967a8bdbd8fb47854855
Daniel Gold <contact@golddaniel.ca>
2025-04-04 23:24:27 -0500

    transition callback

--- Assets/Scripts/SceneTransitions.cs
+++ Assets/Scripts/SceneTransitions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -29,16 +31,16 @@ public class SceneTransitions : MonoSingleton<SceneTransitions>
 		_ => null
 	};
 
-	public void LoadScene(string scene, SceneTransition type)
+	public void LoadScene(string scene, SceneTransition type, Action onTransitionComplete = null)
 	{
 		if(!_inTransition)
 		{
 			_inTransition = true;
-			StartCoroutine(TransitionToScene(scene, type));
+			StartCoroutine(TransitionToScene(scene, type, onTransitionComplete));
 		}
 	}
 
-	private IEnumerator TransitionToScene(string scene, SceneTransition type)
+	private IEnumerator TransitionToScene(string scene, SceneTransition type, Action onTransitionComplete)
 	{
 		var transition = Instantiate(TransitionFor(type));
 		DontDestroyOnLoad(transition);
@@ -47,17 +49,18 @@ public class SceneTransitions : MonoSingleton<SceneTransitions>
 		while (!transition.IsComplete)
 			yield return null;
 
-		transition.Setup(true);
-
 		var currentScene = SceneManager.GetActiveScene();
 		var loadOp = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
 		while (!loadOp.isDone)
 			yield return null;
 
+		transition.Setup(true);
 		while (!transition.IsComplete)
 			yield return null;
 
 		Destroy(transition.gameObject);
 		_inTransition = false;
+
+		onTransitionComplete?.Invoke();
 	}
 }
--- Assets/Scripts/Transitions/WipeLeftTransition.cs
+++ Assets/Scripts/Transitions/WipeLeftTransition.cs
@@ -22,15 +22,16 @@ public class WipeLeftTransition : Transition
 		_initial = reverse ? new Vector3(0, 0, 0) : new Vector3(-20, 0, 0);
 		_target = reverse ? new Vector3(-20, 0, 0) : new Vector3(0, 0, 0);
 
-		
 
+		var quadHeight = Camera.main.orthographicSize * 2.0f;
+		var quadWidth = quadHeight * Screen.width / Screen.height;
 		wipeSquare.transform.position = _initial;
+		wipeSquare.transform.localScale = new Vector3(quadWidth, quadHeight, 1);
 		GetComponent<AudioSource>().Play();
 	}
 
 	public void Update()
 	{
-		
 		wipeSquare.transform.position = Vector3.Lerp(_initial, _target, Easing.InCirc(_t));
 		_t += Time.deltaTime * 2.5f;
 		_t = Mathf.Clamp01(_t);
